ce747e82677a81e32669e6ee6cafab542d2f3434
SmallW <447587073@qq.com>
2019-03-04 11:51:14 +0800

    chang

--- demo/bind.js
+++ demo/bind.js
@@ -1,10 +1,22 @@
-Function.prototype.testBind = function() {
-  let args = this.arguments
-  let that = this
-  return function() {
-    return that.apply(this, arguments)
+// Function.prototype.testBind = function() {
+//   let args = this.arguments
+//   let that = this
+//   return function() {
+//     return that.apply(this, args)
+//   }
+// }
+
+if (!Function.prototype.tbind) {
+  Function.prototype.tbind = function () {
+      var self = this,                        // 保存原函数
+      context = [].shift.call(arguments), // 保存需要绑定的this上下文
+      args = [].slice.call(arguments);    // 剩余的参数转为数组
+      return function () {                    // 返回一个新函数
+          self.apply(context,[].concat.call(args, [].slice.call(arguments)));
+      }
   }
 }
+
 
 var obj = {
   name: 'zhang'
@@ -13,12 +25,10 @@ var obj = {
 var obj1 = {
   name: 'ge',
   getName() {
-    return this.name
+    console.log(this.name)
   }
 }
 
-var gets = obj1.getName.testBind(obj)
+var gets = obj1.getName.tbind(obj)
 
 var res = gets()
-
-console.log(res)
